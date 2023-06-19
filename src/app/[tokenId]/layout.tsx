a9b45463571e41b1462a8dfec8f0aa8f53cdc92e
Don <don@don.dev>
2023-06-19 00:08:03 -0700

    test: remove wagmi

--- src/app/[tokenId]/layout.tsx
+++ src/app/[tokenId]/layout.tsx
@@ -2,11 +2,6 @@
 
 import '../../../styles/globals.css';
 
-import { getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { mainnet, goerli, sepolia, polygon, polygonMumbai } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
 import { useEffect, useState } from 'react';
 
 export default function RootLayout({
@@ -14,29 +9,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // Wagmi configuration
-  const { chains, publicClient } = configureChains(
-    [mainnet, goerli, sepolia, polygon, polygonMumbai],
-    [
-      alchemyProvider({
-        apiKey: (process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string) ?? '',
-      }),
-      publicProvider(),
-    ]
-  );
-
-  const { connectors } = getDefaultWallets({
-    appName: 'My App',
-    chains,
-  });
-
-  const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient,
-  });
-
-  const [_chains, setChains] = useState(chains);
   const [ready, setReady] = useState(false);
   useEffect(() => {
     setReady(true);
@@ -45,13 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen justify-between bg-white z-30">
-        {ready ? (
-          <WagmiConfig config={wagmiConfig}>
-            <div className="flex-grow">{children}</div>
-          </WagmiConfig>
-        ) : (
-          <></>
-        )}
+        {ready ? <div className="flex-grow">{children}</div> : <></>}
       </body>
     </html>
   );
